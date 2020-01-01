28226b83e4519dc1335cdd8618151955d262d3bc
jgohbb <55856950+jgohbb@users.noreply.github.com>
2020-01-01 23:37:44 +0800

    made further adjustmemnts and created env and random files

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var fs = require("fs");
+
